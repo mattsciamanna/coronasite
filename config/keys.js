382797628b3ed49cd9191977314ae233b6cc1d94
Matthew Sciamanna <mattsciamanna@Matthews-MacBook-Air-6.local>
2020-03-18 15:04:27 -0400

    fully functioning, first push, have a few pages and auth system but needs a lot of work still.

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://matt:'+ encodeURIComponent('$Mongo$2020') + '@cluster0-esz1r.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
